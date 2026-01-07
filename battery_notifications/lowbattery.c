476f0a6f03f8c535bde724bfc5cafccfa0541f3a
MartoNievas <tinnievas@gmail.com>
2026-01-06 22:46:50 -0300

    bug window lock fix

--- battery_notifications/lowbattery.c
+++ battery_notifications/lowbattery.c
@@ -3,6 +3,9 @@
 #include <sys/file.h>
 #include <errno.h>
 #include <libnotify/notify.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #define LOW_BATTERY_WARNING_THRESHOLD 20
 #define F_LOW 1
@@ -13,140 +16,126 @@
 char* readfile(char* base, char* file)
 {
     char path[512];
-    char *line = (char*) malloc(512 * sizeof(char));
-    memset(line, 0, 512 * sizeof(char));
+    char *line = malloc(512);
+    if (!line) return NULL;
 
-    snprintf(path, 512, "%s/%s", base, file);
+    memset(line, 0, 512);
+    snprintf(path, sizeof(path), "%s/%s", base, file);
+
     FILE* fp = fopen(path, "r");
-
-    if(fp == NULL) {
+    if (!fp) {
         free(line);
         return NULL;
     }
-    if(fgets(line, 511, fp) == NULL) {
+
+    if (!fgets(line, 511, fp)) {
+        fclose(fp);
         free(line);
         return NULL;
     }
 
     fclose(fp);
-
     return line;
 }
 
-int main() 
+int main()
 {
     int cap;
     int flag = 0;
 
     char file[256];
-    strcat(strcpy(file, getenv("HOME")), "/.lowbattery_lock");
+    strcpy(file, getenv("HOME"));
+    strcat(file, "/.lowbattery_lock");
+
     int pid_file = open(file, O_CREAT | O_RDWR, 0666);
-    int rc = flock(pid_file, LOCK_EX | LOCK_NB);
-    if(rc) {
-        if(EWOULDBLOCK == errno)				// another instance is already running
-        {
-            printf("Error: flock error\n");
+    if (pid_file < 0) return 1;
+
+    if (flock(pid_file, LOCK_EX | LOCK_NB)) {
+        if (errno == EWOULDBLOCK)
             return 1;
-        }
     }
 
-    // Check for path where battery data is saved
-    // Some laptops will have it set in BAT0 & some in BAT1
-    char path[64] = { 0 };
-    FILE* fp = fopen("/sys/class/power_supply/BAT0", "r");
-    if (fp != NULL)
-        strcat(path, "/sys/class/power_supply/BAT0");
-    else
-        strcat(path, "/sys/class/power_supply/BAT1");
-
-    if (fp)
+    char path[64] = {0};
+    FILE* fp = fopen("/sys/class/power_supply/BAT0/capacity", "r");
+    if (fp) {
+        strcpy(path, "/sys/class/power_supply/BAT0");
         fclose(fp);
-    NotifyNotification* notification = NULL;
-    if (!notify_init("Low battery notification")) {
-      printf("Error: notify_init \n");
-      return 0;
-    } 
-    for (;;sleep(1)) 
-    {
-        // Read current battery capacity
+    } else {
+        strcpy(path, "/sys/class/power_supply/BAT1");
+    }
+
+    if (!notify_init("Low battery notification"))
+        return 1;
+
+    for (;; sleep(1)) {
+
         char* cp = readfile(path, "capacity");
-        if (!cp) continue;
+        if (!cp)
+            continue;
+
         sscanf(cp, "%d", &cap);
         free(cp);
- 
-        // Read current battery status (Charing/Discharging)
+
         cp = readfile(path, "status");
-        if (!cp) continue;
-        //UnSet flag FULL
-        
-        if(!strncmp(cp, "Discharging", 11))
-        {
-            // If discharing and batery cap is less than threshold then send low battey notif
-            if(cap <= LOW_BATTERY_WARNING_THRESHOLD && (flag & F_LOW) == 0)
-            {
-                // Set flag to one which means notification has been sent and won't bother you again
+        if (!cp)
+            continue;
+
+        if (!strncmp(cp, "Discharging", 11)) {
+
+            if (cap <= LOW_BATTERY_WARNING_THRESHOLD && !(flag & F_LOW)) {
                 flag |= F_LOW;
-    
-                notification = notify_notification_new("Battery Low", "Connect charger", NULL);
-                notify_notification_set_urgency(notification, NOTIFY_URGENCY_CRITICAL);
-                notify_notification_show(notification,NULL);
-                g_object_unref(notification);
- 
-            } 
-            if ( (flag & F_CHG)) {
-                flag &= ~F_CHG; // set to flag not charge
-                notification = notify_notification_new("Discharging", "Charger disconnect",NULL);
-                notify_notification_show(notification,NULL);
-                g_object_unref(notification);
-            }    
-        } 
-        else if(!strncmp(cp, "Charging", 8))
-        {   
-            //Cambiamos la flag de lo ya que esta cargando
-            if((flag & F_LOW))
-            {
+                NotifyNotification* notify =
+                    notify_notification_new("Battery Low", "Connect charger", NULL);
+                notify_notification_set_urgency(notify, NOTIFY_URGENCY_CRITICAL);
+                notify_notification_show(notify, NULL);
+                g_object_unref(notify);
+            }
+
+            if (flag & F_CHG) {
+                flag &= ~F_CHG;
+                flag &= ~F_FULL_CHG;
+                NotifyNotification* notify =
+                    notify_notification_new("Discharging", "Charger disconnect", NULL);
+                notify_notification_show(notify, NULL);
+                g_object_unref(notify);
+            }
+
+        } else if (!strncmp(cp, "Charging", 8)) {
+            if ((flag & F_LOW))
                 flag &= ~F_LOW;
+
+            if (cap < 100)
+                flag &= ~F_FULL; 
+
+            if (!(flag & F_CHG)) {
+                flag |= F_CHG;
+                flag &= ~F_FULL_CHG;
+                NotifyNotification* notify =
+                    notify_notification_new("Charging", "Charger connect", NULL);
+                notify_notification_show(notify, NULL);
+                g_object_unref(notify);
             }
-            //Si la bateria no esta full cabiamos ambas flags de full
-            if (cap < 100 ) {
-              flag &= ~F_FULL;
-            }
-            //Seteamos full battry + cargando 
-            if (cap == 100 && (flag & F_FULL_CHG) && !(flag & F_CHG)) {
-              flag &= ~F_FULL_CHG;
-            }
-            //Notificacion de cargador conectado 
-            if ((flag & F_CHG) == 0) {
-                flag |= F_CHG; //Set flag charge 
-                notification = notify_notification_new("Charging", "Charger connect",NULL); 
-                notify_notification_show(notification,NULL);
-                g_object_unref(notification);
-            }
-            //Notificacion de bateria full
-            if (cap == 100 && ((flag & F_FULL) == 0)) {
+
+            if (cap == 100 && !(flag & F_FULL)) {
                 flag |= F_FULL;
-                notification = notify_notification_new("Battery full", "Disconnect charger",NULL);
-                notify_notification_show(notification,NULL);
-                g_object_unref(notification);
+                NotifyNotification* notify =
+                    notify_notification_new("Battery full", "Disconnect charger", NULL);
+                notify_notification_show(notify, NULL);
+                g_object_unref(notify);
             }
-            // Notificación batería full + cargador conectado
-            if(cap == 100 && (flag & F_CHG) && ((flag & F_FULL_CHG) == 0)) {
+
+            if (cap == 100 && (flag & F_CHG) && !(flag & F_FULL_CHG)) {
                 flag |= F_FULL_CHG;
-                notification = notify_notification_new("Battery full", "Charger still connected", NULL);
-                notify_notification_show(notification,NULL);
-                g_object_unref(notification);
+                NotifyNotification* notify =
+                    notify_notification_new("Battery full", "Charger still connected", NULL);
+                notify_notification_show(notify, NULL);
+                g_object_unref(notify);
             }
-
-        } 
-        {   
-           
-				    notification = NULL;
         }
-        free(cp);
+
+        free(cp);   
     }
+
     notify_uninit();
-
-
     return 0;
 }
-
