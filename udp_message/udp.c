c925fedbeabdc114157011ff7f81f1b53006d577
MartoNievas <tinnievas@gmail.com>
2026-01-20 19:51:28 -0300

    complete mode

--- udp_message/udp.c
+++ udp_message/udp.c
@@ -5,28 +5,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #define BUF_SIZE 256
 
-int main(int argc, char *argv[]) {
-
-  bool listen_mode = false;
-  int port = 0;
-
+static void check_mode(int argc, char *argv[], bool *listen_mode,
+                       bool *complete_mode, int *port) {
   if (argc == 3 && strcmp(argv[1], "-l") == 0) {
-    listen_mode = true;
-    port = atoi(argv[2]);
+    *listen_mode = true;
+    *port = atoi(argv[2]);
   } else if (argc == 5 && strcmp(argv[1], "-d") == 0 &&
              strcmp(argv[3], "-p") == 0) {
-    port = atoi(argv[4]);
+    *port = atoi(argv[4]);
+    // ./ udp -d <device_ip> -p <port>
+  } else if (argc == 5 && strcmp(argv[1], "-d") == 0 &&
+             strcmp(argv[argc - 2], "-l") == 0) {
+    *complete_mode = true;
+    *port = atoi(argv[argc - 1]);
   } else {
     printf("Usage:\n");
     printf("  %s -l <port>\n", argv[0]);
-    printf("  %s -d <device|ip> -p <port>\n", argv[0]);
-    return 1;
+    printf("  %s -d <device_ip> -p <port>\n", argv[0]);
+    printf("  %s -d <device_ip> -l <port>\n", argv[0]);
+    exit(1);
   }
+}
+
+int main(int argc, char *argv[]) {
+
+  bool listen_mode = false;
+  bool complete_mode = false;
+  int port = 0;
+
+  check_mode(argc, argv, &listen_mode, &complete_mode, &port);
 
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sockfd < 0) {
@@ -74,6 +87,94 @@ int main(int argc, char *argv[]) {
       printf("From %s:%d -> %s\n", inet_ntoa(client_addr.sin_addr),
              ntohs(client_addr.sin_port), buffer);
     }
+
+    /* ================= COMPLETE MODE ================= */
+  } else if (complete_mode) {
+
+    struct sockaddr_in local_addr;
+    struct sockaddr_in remote_addr;
+    fd_set readfds;
+    char buffer[BUF_SIZE];
+    char *ip = argv[2];
+    socklen_t addr_len = sizeof(remote_addr);
+
+    bzero(&local_addr, sizeof(local_addr));
+    bzero(&remote_addr, sizeof(remote_addr));
+
+    /*local addr */
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = INADDR_ANY;
+    local_addr.sin_port = htons(port);
+
+    /* remote addr */
+    remote_addr.sin_family = AF_INET;
+    remote_addr.sin_port = htons(port);
+
+    if (inet_aton(ip, &remote_addr.sin_addr) == 0) {
+      printf("Invalid IP address\n");
+      close(sockfd);
+      return 1;
+    }
+
+    if (bind(sockfd, (struct sockaddr *)&local_addr, sizeof(local_addr)) < 0) {
+      perror("bind");
+      close(sockfd);
+      return 1;
+    }
+
+    printf("Listening on port %d...\n", port);
+    printf("Ready to communicate with %s:%d\n", ip, port);
+    printf("Message: ");
+    fflush(stdout);
+
+    for (;;) {
+      FD_ZERO(&readfds);
+      FD_SET(sockfd, &readfds);
+      FD_SET(STDIN_FILENO, &readfds);
+
+      int maxfd = sockfd > STDIN_FILENO ? sockfd : STDIN_FILENO;
+
+      if (select(maxfd + 1, &readfds, NULL, NULL, NULL) < 0) {
+        perror("select");
+        break;
+      }
+
+      if (FD_ISSET(sockfd, &readfds)) {
+        struct sockaddr_in from_addr;
+        socklen_t from_len = sizeof(from_addr);
+
+        int n = recvfrom(sockfd, buffer, BUF_SIZE - 1, 0,
+                         (struct sockaddr *)&from_addr, &from_len);
+
+        if (n > 0) {
+          buffer[n] = '\0';
+
+          if (buffer[n - 1] == '\n') {
+            buffer[n - 1] = '\0';
+          }
+
+          printf("\nFrom %s:%d -> %s\n", inet_ntoa(from_addr.sin_addr),
+                 ntohs(from_addr.sin_port), buffer);
+          printf("Message: ");
+          fflush(stdout);
+        } else if (n < 0) {
+          perror("recvfrom");
+        }
+      }
+
+      if (FD_ISSET(STDIN_FILENO, &readfds)) {
+        if (!fgets(buffer, BUF_SIZE, stdin))
+          break;
+
+        int sent = sendto(sockfd, buffer, strlen(buffer), 0,
+                          (struct sockaddr *)&remote_addr, sizeof(remote_addr));
+        if (sent < 0) {
+          perror("sendto");
+        }
+        printf("Message: ");
+        fflush(stdout);
+      }
+    }
   }
 
   /* ================= SEND MODE ================= */
@@ -88,13 +189,6 @@ int main(int argc, char *argv[]) {
 
     if (inet_aton(ip, &server_addr.sin_addr) == 0) {
       printf("Invalid IP address\n");
-      close(sockfd);
-      return 1;
-    }
-
-    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
-        0) {
-      perror("connect");
       close(sockfd);
       return 1;
     }
